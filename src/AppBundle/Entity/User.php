894676db44e3deb5c13118d170cfe73fe332c54a
bycrea <yoan.despert@lapiscine.pro>
2019-05-01 15:45:01 +0200

    user entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -7,11 +7,16 @@ use Doctrine\ORM\Mapping as ORM;
 
 
 /**
- * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
 class User extends BaseUser
 {
+    const GENDER_MAN = 0;
+    const GENDER_FEM = 1;
+    const GENDER_OTHER = 2;
+    const GENDERS = [self::GENDER_MAN => 'Homme', self::GENDER_FEM => 'Femme', self::GENDER_OTHER => 'Autre'];
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -19,9 +24,166 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday_at", type="date")
+     */
+    private $birthdayAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="gender", type="integer")
+     */
+    private $gender;
+
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set birthdayAt
+     *
+     * @param \DateTime $birthdayAt
+     *
+     * @return User
+     */
+    public function setBirthdayAt($birthdayAt)
+    {
+        $this->birthdayAt = $birthdayAt;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdayAt
+     *
+     * @return \DateTime
+     */
+    public function getBirthdayAt()
+    {
+        return $this->birthdayAt;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param integer $gender
+     *
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return integer
+     */
+    public function getGender()
+    {
+        return $this->gender;
     }
 }
